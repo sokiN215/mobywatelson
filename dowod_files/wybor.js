5285a94af8eed681af0f319e033e2b4d6abc66f3
zazaman420-git <novexu69@gmail.com>
2025-11-06 14:33:42 +0100

    Add files via upload

---
+++ dowod_files/wybor.js
@@ -0,0 +1,38 @@
+$('body').removeClass('ovh');
+$('#wybordokumentu').hide();
+$('#menu').hide();
+$('#danedowodu').hide();
+$('#danemdowodu').hide();
+
+function wybordokumentumenu() {
+    window.scrollTo({
+      top: 0
+    });
+    $('body').toggleClass('ovh');
+    $('#wybordokumentu').toggle();
+    $('#menu').toggle();
+}
+
+function danedowodowe() {
+  $('#wybordokumentu').hide();
+  $('#danedowodu').show();
+}
+
+function danemdowodowe() {
+  $('#wybordokumentu').hide();
+  $('#danemdowodu').show();
+}
+
+function zamknij2() {
+  $('#danedowodu').hide();
+  $('#danemdowodu').hide();
+  $('#menu').hide();
+  $('body').removeClass('ovh');
+}
+
+
+function wroc(){
+  $('#wybordokumentu').show();
+  $('#danedowodu').hide();
+  $('#danemdowodu').hide();
+}
